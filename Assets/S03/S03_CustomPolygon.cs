4bfa4c4b70921b77a24561f65739209c567abeed
chobabssi <gse08090@naver.com>
2026-09-19 21:13:48 +0900

    S03 삼각형 winding order 수정

--- Assets/S03/S03_CustomPolygon.cs
+++ Assets/S03/S03_CustomPolygon.cs
@@ -24,12 +24,12 @@ public class S03_CustomPolygon : MonoBehaviour
         int[] triangles = new int[]
         {
               // 예: 0, 1, 2,
-            2,1,0,
-            2,0,7,
-            2,7,6,
-            2,6,5,
-            2,5,4,
-            2,4,3
+            2,0,1,
+            2,7,0,
+            2,6,7,
+            2,5,6,
+            2,4,5,
+            2,3,4
 
 
         };
